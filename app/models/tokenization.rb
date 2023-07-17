fc6e964e19ac0a9ed43cf32ac32115972d0ae9f6
Alexandre Gueths <agueths@gmail.com>
2023-07-16 21:02:55 -0300

    RSpec base config

--- app/models/tokenization.rb
+++ app/models/tokenization.rb
@@ -4,51 +4,62 @@ class Tokenization
   def self.encode(payload)
     timenow = Time.now.to_i
     JWT.encode(
-      payload.merge(
-        {
-          exp: (timenow + 4 * 3600),
-          iat: timenow,
-          jti: Digest::MD5.hexdigest(
-            [
-              Rails.application.credentials[:jwt_secret_key],
-              timenow
-            ].join(':').to_s
-          )
-        }
-      ),
-      Rails.application.credentials[:jwt_secret_key],
+      {
+        exp: (timenow + 4 * 3600),
+        iat: timenow,
+        jti: Digest::MD5.hexdigest(
+          [
+            Rails.application.credentials.jwt_secret_key,
+            timenow
+          ].join(':').to_s
+        ),
+        data: payload
+      },
+      Rails.application.credentials.jwt_secret_key,
       'HS512'
     )
   end
 
   def self.decode(token)
+    jwt_ret = JWT.decode(
+      token,
+      Rails.application.credentials.jwt_secret_key,
+      true,
+      {
+        verify_iat: true, verify_jti: true, algorithm: 'HS512'
+      }
+    )[0]
     {
       error: false,
-      payload: JWT.decode(
-        token,
-        Rails.application.credentials[:jwt_secret_key],
-        true, {
-          verify_expiration: true, verify_iat: true, verify_jti: true, algorithm: 'HS512'
-        }
-      )[0]
+      payload: jwt_ret['data']
     }
-  rescue JWT::ExpiredSignature
+  rescue JWT::ExpiredSignature => e
     {
       error: true,
       command: 'tokenization_decode',
-      raised: 'expired_signature'
+      raised: 'expired_signature',
+      message: e
     }
-  rescue JWT::InvalidIatError
+  rescue JWT::InvalidIatError => e
     {
       error: true,
       command: 'tokenization_decode',
-      raised: 'invalid_iat'
+      raised: 'invalid_iat',
+      message: e
     }
-  rescue JWT::InvalidJtiError
+  rescue JWT::InvalidJtiError => e
     {
       error: true,
       command: 'tokenization_decode',
-      raised: 'invalid_jti'
+      raised: 'invalid_jti',
+      message: e
+    }
+  rescue JWT::DecodeError => e
+    {
+      error: true,
+      command: 'tokenization_decode',
+      raised: 'decode_error',
+      message: e
     }
   end
 end
